965e7109d59aa717c67f1bf6550a062bd8725ae8
Руслан Хазиахметов <ruslanhaziahmetov@MacBook-Pro-Ruslan.local>
2022-01-17 13:45:00 +0500

    fix design

--- src/components/Form/FormResetPassword.js
+++ src/components/Form/FormResetPassword.js
@@ -2,7 +2,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {useForm} from "react-hook-form";
 import * as yup from 'yup';
 import {yupResolver} from "@hookform/resolvers/yup";
-import {TextField,Button} from "@mui/material";
+import {TextField,Button,Stack} from "@mui/material";
 import React from "react";
 import {actionsResetPassword} from "../../store/actions/actionsAuthUser";
 
@@ -20,21 +20,23 @@ function FormResetPassword({login}){
         dispatch(actionsResetPassword({login:login,...data}))
     };
     return(
-        <form action="" onSubmit={handleSubmit(onSubmitReset)}>
-            <TextField
-                variant="standard"
-                placeholder="Введите новый пароль"
-                {...register('resetPassword')}
-            />
-            <TextField
-                variant="standard"
-                placeholder="Подвердите новый пароль"
-                {...register('confirmResetPassword')}
-            />
-            <Button variant="contained" type="submit">
-                Сменить пароль
-            </Button>
-        </form>
+            <form action="" onSubmit={handleSubmit(onSubmitReset)}>
+                <Stack spacing={2} direction="column">
+                <TextField
+                    variant="standard"
+                    placeholder="Введите новый пароль"
+                    {...register('resetPassword')}
+                />
+                <TextField
+                    variant="standard"
+                    placeholder="Подвердите новый пароль"
+                    {...register('confirmResetPassword')}
+                />
+                <Button variant="contained" type="submit">
+                    Сменить пароль
+                </Button>
+                </Stack>
+            </form>
     )
 }
 
