59105604285573ad5eaa287c87e69022d7711a1c
ruslanhaziahmetov <ruslan.xaziaxmetov@mail.ru>
2022-02-15 16:30:25 +0500

    "Изменил select на material-ui"

---
+++ src/components/Autocomplete/Autocomplete.js
@@ -0,0 +1,52 @@
+import {Autocomplete, TextField,createFilterOptions} from "@mui/material";
+import React from "react";
+
+
+export default function CustomAutoComplete({options,onChange,value,defaultValue = ""}){
+    console.log(defaultValue)
+    const filter = createFilterOptions()
+    return(
+        <Autocomplete
+            value={value}
+            defaultValue={defaultValue}
+            options={options}
+            onChange={(event,newValue)=>{
+                if(newValue && newValue.inputValue){
+                    onChange(newValue?.label)
+                }
+                onChange(newValue?.label)
+            }}
+            filterOptions={(options,params)=>{
+                const filtered = filter(options,params)
+                const {inputValue} = params
+                const isExisting = options.some((option)=>inputValue === option.label)
+
+                if(inputValue !== '' && !isExisting){
+                    filtered.push({
+                        label:inputValue
+                    })
+                }
+                return filtered
+            }}
+            getOptionLabel={(option)=>{
+                if(typeof option === 'string'){
+                    return option
+                }
+                if(option.inputValue){
+                    return option.inputValue
+                }
+                return option.label
+            }}
+            renderOption={(props,option)=><li {...props}>{option.label}</li>}
+            selectOnFocus
+            clearOnBlur
+            handleHomeEndKeys
+            renderInput={(params)=>(
+                <TextField
+                    {...params}
+                    label="Выберите организацию..."
+                />
+            )}
+        />
+    )
+}
